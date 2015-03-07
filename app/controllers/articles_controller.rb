326445b9b50c297d1840a155df669599882d780d
qq2441570482 <2441570482@qq.com>
2015-03-07 18:03:15 +0800

    blog 2.2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,26 @@
 class ArticlesController < ApplicationController
   before_action :require_login, only: [:index,:new,:create,:edit]
-  before_action :find_article, only: [:show,:edit,:update,:destroy]
+  before_action :find_article, only: [:edit,:update,:destroy]
   before_action :new_article, only: [:new]
   before_action :all_articles, only: [:show]
+  before_action :all_tags, only: [:edit, :new]
 
 
   def index
     @articles = Article.all.order(created_at: :desc)
   end
 
-  def new
-    @tags = Tag.all
+  def show
+    Article.exists?(params[:id]) ? @article = Article.find(params[:id]) : @article = nil
+
   end
+
 
   def create
     article = Article.new(article_params)
     if article.save
         params[:article][:tags].each do |article_tag|
-          if article_tag != ''
             ArticleTag.create(article_id: article.id, tag_id: article_tag.to_i )
-          end
         end
       redirect_to articles_path
     else
@@ -55,6 +56,10 @@ class ArticlesController < ApplicationController
 
   def all_articles
     @articles = Article.all.order(created_at: :desc)
+  end
+
+  def all_tags
+    @tags = Tag.all
   end
 
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,7 @@ class WelcomeController < ApplicationController
      if params[:condition].present?
      	@index_articles = @articles.where('title like ?', '%'+ params[:condition]+ '%')
      end
+
   end
 
   def about
@@ -13,11 +14,11 @@ class WelcomeController < ApplicationController
   end
 
   def tag
-    @tag = Tag.find(params[:id])
+    @tag = Tag.exists?(params[:id]) ? Tag.find(params[:id]) : nil
   end
 
   private
   def all_articles
-    @articles = Article.all.order(created_at: :desc)
+    @articles = Article.all.order(created_at: :desc).limit(10)
   end
 end
