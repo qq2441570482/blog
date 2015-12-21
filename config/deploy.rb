d2a02342cd6ca8e5a70fa8d3946ecb0d482f67e8
Yanqing Wang <2441570482@qq.com>
2015-12-21 14:46:31 +0800

    test 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ set :deploy_to, '/home/ubuntu'
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
-# set :default_env, { path: '$PATH:/home/ubuntu/.rvm/gems/ruby-2.1.4/bin/'}
+set :default_env, { path: '/home/ubuntu/.rvm/gems/ruby-2.1.4/bin/:$PATH'}
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
