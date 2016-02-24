5c36c0748228a1241fd50bbe6dfc9a1d916524f7
Yanqing Wang <2441570482@qq.com>
2016-02-24 22:51:47 +0800

    format code

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
 
-	has_many :article_tags, dependent: :destroy
-	has_many :articles, through: :article_tags
+  has_many :article_tags, dependent: :destroy
+  has_many :articles, through: :article_tags
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-	has_secure_password
+  has_secure_password
 
-	validates :password, confirmation: true
-	validates :password_confirmation, presence: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
